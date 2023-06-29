ae8fa9ced0c07a0da839e92e19e59fff8c5905e9
Luis Ignacio Gonzalez Silva <luis_gosi@outlook.com>
2023-06-29 07:16:36 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var path = require("path");
+var session = require("express-session");
+var usuariosRutas = require("./rutas/usuarios");
+require("dotenv").config();
+
+var app = express();
+app.set("view engine", "ejs");
+app.use("/web", express.static(path.join(__dirname, "/web")));
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    secret: process.env.SECRETO_SESSIONS,
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+app.use("/", usuariosRutas);
+
+var port = process.env.PUERTO || 3000;
+
+app.listen(port, () => {
+  console.log(`Servidor en http://localhost:${port}`);
+});
